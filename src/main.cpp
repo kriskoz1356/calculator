7c524a45c9f7ef31620caa11714c93488cf54786
adamf <adamf@PC-TAKASANEK75>
2026-09-13 16:22:29 +0200

    deleted some couts

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ int main() {
     std::cin >> a >> b;
 
     std::cout << "Addition: " << calc.add(a, b) << std::endl;
-    std::cout << "Subtraction: " << calc.subtract(a, b) << std::endl;
-    std::cout << "Multiplication: " << calc.multiply(a, b) << std::endl;
-    std::cout << "Division: " << calc.divide(a, b) << std::endl;
+
     std::cout << "Modulus: " << calc.modulus(a, b) << std::endl;
 
-    
+
     return 0;
 }
