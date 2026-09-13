3439f5eb1df11752ff17c2a980d7633987ac9cac
adamf <adamf@PC-TAKASANEK75>
2026-09-13 16:24:27 +0200

    added some couts

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@ int main() {
 
     std::cout << "Enter two numbers: ";
     std::cin >> a >> b;
-
     std::cout << "Addition: " << calc.add(a, b) << std::endl;
-
+    std::cout << "Subtraction: " << calc.subtract(a, b) << std::endl;
+    std::cout << "Multiplication: " << calc.multiply(a, b) << std::endl;
+    std::cout << "Division: " << calc.divide(a, b) << std::endl;
     std::cout << "Modulus: " << calc.modulus(a, b) << std::endl;
 
 
